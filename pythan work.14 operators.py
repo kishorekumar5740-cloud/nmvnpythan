1a5f3658f9ea6b2a174ecf99ba072ff943cf8dd4
kishorekumar5740-cloud <kishorekumar5740@gmail.com>
2026-02-09 16:09:43 +0530

    file added

---
+++ pythan work.14 operators.py
@@ -0,0 +1,34 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=10
+>>> b=3
+>>> c=a+b
+>>> print("addition:",c)
+addition: 13
+>>> addition: 13
+>>> 
+>>> c=a-b
+>>> print("subration:",c)
+subration: 7
+>>> 
+>>> c=a*b
+>>> print("mulipy:",c)
+mulipy: 30
+>>> 
+>>> c=a/b
+>>> print("divide:",c)
+divide: 3.3333333333333335
+>>> 
+>>> 
+>>> c=a//b
+>>> print("modules:",c)
+modules: 3
+>>> 
+>>> c=a%b
+>>> print("modulo:",c)
+modulo: 1
+>>> modulo: 1
+>>> 
+>>> c=a**b
+>>> print("expoencial:",c)
+expoencial: 1000
